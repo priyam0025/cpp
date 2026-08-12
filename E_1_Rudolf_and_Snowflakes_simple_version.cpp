675df9370e3ac77512f1281b3232fa8741739397
priyam0025 <priyam98323@gmail.com>
2026-08-12 18:52:40 +0530

    11 and 12 august

---
+++ E_1_Rudolf_and_Snowflakes_simple_version.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#pragma GCC optimize("Ofast,unroll-loops")
+#pragma GCC target("avx2,popcnt")
+
+#define int long long
+#define endl '\n'
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define vpii vector<pair<int, int>>
+#define pb push_back
+#define pii pair<int, int>
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+#define fi first
+#define se second
+#define rep(i, n) for(int i = 0; i < n; i++)
+#define rep1(i, n) for(int i = 1; i <= n; i++)
+#define FOR(i, a, b) for(int i = a; i < b; i++)
+#define maxel(x) (*max_element(all(x)))
+#define minel(x) (*min_element(all(x)))
+#define let(n) int n; cin >> n;
+#define let2(a, b) int a, b; cin >> a >> b;
+#define let3(a, b, c) int a, b, c; cin >> a >> b >> c;
+#define leta(arr, n) vi arr(n); for(int i = 0; i < n; i++) cin >> arr[i];
+#define YES cout << "YES\n"
+#define NO cout << "NO\n"
+#define print(...) cout << __VA_ARGS__ << endl
+#define YN(f) cout << (f ? "YES" : "NO") << endl
+
+const int INF = 2e18;
+const int mod = 1e9 + 7;
+const int mod2 = 998244353;
+
+int ipow(int x, int y) {
+    int res = 1;
+    while (y) {
+        if (y & 1) res *= x;
+        x *= x;
+        y >>= 1;
+    }
+    return res;
+}
+
+void printa(const vi &a) {
+    for (auto x : a) cout << x << ' ';
+    cout << endl;
+}
+
+
+int32_t main() {
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    int tt = 1;   cin >> tt;  // Uncomment for multiple test cases
+    vi check(1e6 + 1, 0);
+
+    FOR(k, 2, 10000) {
+        int cur = k * k * k;
+        int total = (cur - 1) / (k - 1);
+        while (total <= 1e6) {
+            check[total] = 1;
+            cur *= k;
+            total = (cur - 1) / (k - 1);
+        }
+    }
+    for (int t = 1; t <= tt; t++) {
+        let(n);
+        YN(check[n]);
+    }
+    return 0;
+}
