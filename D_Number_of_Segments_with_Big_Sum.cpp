e0f95ea2a46930263ebd5744a6c2fb6a6408ab06
priyam0025 <priyam98323@gmail.com>
2026-08-21 00:58:47 +0530

    19, 20, 21 aug

--- D_Number_of_Segments_with_Big_Sum.cpp
+++ D_Number_of_Segments_with_Big_Sum.cpp
@@ -1,36 +1,68 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// #define int long long
+
+// void run() {
+//     int n, s;
+//     cin >> n >> s;
+//     vector<int> a(n);
+//     for (int i = 0; i < n; ++i) cin >> a[i];
+//     auto bro = [&](vector<int> a, int x) -> int {
+//         int l = 0, r = 0;
+//         int sum = 0;
+//         int cnt = 0;
+//         while (r < a.size()) {
+//             sum += a[r];
+//             while (sum >= x) {
+//                 cnt += (n - r);
+//                 sum -= a[l];
+//                 l++;
+//             }
+//             r++;
+//         }
+//         return cnt;
+//     };
+//     int ans = bro(a, s);
+//     cout << ans << '\n';
+// }
+
+// int32_t main() {
+//     ios::sync_with_stdio(false); cin.tie(nullptr);
+//     int tt = 1;
+//     // cin >> tt;
+//     while (tt--) run();
+//     return 0;
+// }
+
 #include <bits/stdc++.h>
 using namespace std;
-
-#define int long long
-
-void run() {
-    int n, s;
+int main()
+{
+    long long n, s;
     cin >> n >> s;
-    vector<int> a(n);
-    for (int i = 0; i < n; ++i) cin >> a[i];
-    auto bro = [&](vector<int> a, int x) -> int {
-        int l = 0, r = 0;
-        int sum = 0;
-        int cnt = 0;
-        while (r < a.size()) {
-            sum += a[r];
-            while (sum >= x) {
-                cnt += (n - r);
-                sum -= a[l];
-                l++;
-            }
-            r++;
+    vector<long long> nums(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> nums[i];
+    }
+    int r = 0;
+    int l = 0;
+    long long sum = 0;
+    long long count = 0;
+    while (r < nums.size())
+    {
+        sum += nums[r];
+        while (sum >= s)
+        {
+            sum -= nums[l];
+            l++;
         }
-        return cnt;
-    };
-    int ans = bro(a, s);
-    cout << ans << '\n';
-}
-
-int32_t main() {
-    ios::sync_with_stdio(false); cin.tie(nullptr);
-    int tt = 1;
-    // cin >> tt;
-    while (tt--) run();
+        count += (r - l + 1);
+        r++;
+    }
+    long long total_sub_arrays = n * (n + 1) / 2;
+    long long result = total_sub_arrays - count;
+    cout << result;
     return 0;
 }
